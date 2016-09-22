ce7e61253210932743671bb0fc4d500854d2b589
a1500921 <a1500921@pao5009x22.haagahelia.amk>
2016-09-22 10:11:53 +0300

    database-folder lisätty + createt ja insertit

---
+++ sovellus/database/create_tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE AKTIVITEETTI (
+aktiivi_id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+aktiivi_nimi VARCHAR(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE TAPAHTUMA (
+tapahtuma_id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+osallistujamaara SMALLINT,
+max_osallistujamaara SMALLINT,
+tapahtuma_nimi VARCHAR(255),
+jar_nimi VARCHAR(255),
+puh_num VARCHAR(10),
+email VARCHAR(255),
+tapahtuma_aika DATETIME,
+kuvaus VARCHAR(255),
+tapahtuma_paikka VARCHAR(255),
+aktiivi_id SMALLINT,
+FOREIGN KEY (aktiivi_id) REFERENCES AKTIVITEETTI (aktiivi_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ sovellus/database/insert_test_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO AKTIVITEETTI 
+	(aktiivi_nimi)
+VALUES
+	('Jalkapallo'),
+	('Kutominen'),
+	('Salibandy'),
+	('Ruoanlaitto');
+
+INSERT INTO TAPAHTUMA
+	(osallistujamaara, max_osallistujamaara, tapahtuma_nimi, jar_nimi, puh_num, email, tapahtuma_aika, kuvaus, tapahtuma_paikka, aktiivi_id)
+VALUES
+	('0','2','Jalkapalloa','Juha Kuikka','0401234567','juha-kuikka@sposti.fi','2016-10-10 16:00:00','Tule pelaamaan jalkapalloa pienen porukan kanssa!','Käpylän urheilukenttä','1');

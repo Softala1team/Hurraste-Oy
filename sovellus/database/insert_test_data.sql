e2fdd47faf66c44b5d4670c9ff32a2c06188a625
a1500921 <a1500921@paom5009xp22.haagahelia.amk>
2016-11-08 09:04:27 +0200

    database taulut

--- sovellus/database/insert_test_data.sql
+++ sovellus/database/insert_test_data.sql
@@ -7,6 +7,6 @@ VALUES
 	('Ruoanlaitto');
 
 INSERT INTO TAPAHTUMA
-	(osallistujamaara, max_osallistujamaara, tapahtuma_nimi, jar_nimi, puh_num, email, tapahtuma_aika, kuvaus, tapahtuma_paikka, aktiivi_id)
+	(osallistujamaara, max_osallistujamaara, tapahtuma_nimi, jar_nimi, puh_num, email, tapahtuma_aika, loppumis_aika, kuvaus, tapahtuma_paikka, aktiivi_id)
 VALUES
-	('0','2','Jalkapalloa','Juha Kuikka','0401234567','juha-kuikka@sposti.fi','2016-10-10 16:00:00','Tule pelaamaan jalkapalloa pienen porukan kanssa!','Käpylän urheilukenttä','1');
+	('0','2','Jalkapalloa','Juha Kuikka','0401234567','juha-kuikka@sposti.fi','2016-11-10 16:00:00','2016-11-10 17:00:00','Tule pelaamaan jalkapalloa pienen porukan kanssa!','Käpylän urheilukenttä','1');
